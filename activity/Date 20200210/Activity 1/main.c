5888430cf0a1a76a4fa308abac7a51fa3ef2cc47
AlbertoParente <albertoparente20@gmail.com>
2020-03-15 23:05:43 -0300

    Adjusting in Activit1 date 20200210

--- activity/Date 20200210/Activity 1/main.c
+++ activity/Date 20200210/Activity 1/main.c
@@ -10,7 +10,8 @@ int main(int argc, char *argv[]) {
 	float sumTemperature, biggerFrequency, difference;
 	int i;
 	
-	printf("Informe os dados: \n");
+	printf("\t\t-- Informe os dados a seguir -- \n");
+	printf("\n");
 	for(i=0; i<qtd; i++){
 		printf("Informe sua frequencia cardiaca: \n", i);
 		scanf("%f",&heartRate[i]);
@@ -21,16 +22,19 @@ int main(int argc, char *argv[]) {
 		
 		bodyTemperature[i] = bodyTemperature[i] + sumTemperature;
 		
-		if (heartRate[i] > heartRate){
+		if (heartRate[i] > biggerFrequency){
 			biggerFrequency = heartRate[i];
 		}
 	}
 	
-	difference = terrainElevation [0] - terrainElevation[qtd-1]
+	difference = terrainElevation [0] - terrainElevation[qtd-1];
 	sumTemperature = sumTemperature / qtd;
-	printf("A media da temperatura e: \n", sumTemperature);
-	printf("Sua maior frequencia cardiaca e: \n", biggerFrequency);
-	printf("A diferenca de elevação final referente a inicial e: \n", biggerFrequency);
+	printf("\n");
+	printf("*****************************************************************************\n");
+	printf("\n");
+	printf("A media da temperatura e: %f\n", sumTemperature);
+	printf("Sua maior frequencia cardiaca e: %f\n", biggerFrequency);
+	printf("A diferenca de elevacao final referente a inicial e: %f\n", biggerFrequency);
 
 	return 0;
 }
