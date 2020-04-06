67a2bfdf003955fd626165f46797cfc97084994b
AlbertoParente <albertoparente20@gmail.com>
2020-04-05 22:56:25 -0300

    Adding functions

--- queue/Activity 1/main.c
+++ queue/Activity 1/main.c
@@ -1,8 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "queue.h"
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	return 0;
+void printValueWithoutEmptying(TQueue* queue) {
+	int i;
+	int sizeQueue = queueSize(queue);
+	int valueDequeue;
+	TQueue* queueAux = neww();
+	for(i = 0; i < sizeQueue; i++) {
+		valueDequeue = dequeue(queue);
+		printf("\n%i\n", valueDequeue);
+		enqueue(queueAux, valueDequeue);
+		enqueue(queue, dequeue(queueAux));
+	}
+}
+
+void printEmptyingValue(TQueue* queue) {
+	int i;
+	int sizeQueue = queueSize(queue);
+	int valueDequeue;
+	for(i = 0; i < sizeQueue; i++) {
+		valueDequeue = dequeue(queue);
+		printf("\n%i\n", valueDequeue);
+	}
+}
+/*
+void removeValue(TQueue* queue, int input) {
+	TQueue* queueAux = neww();
+	int i, j;
+	int sizeQueue = queueSize(queue);
+	for(i = 0; i < sizeQueue; i++){
+		if (queueFrontPosition(queue) == input) {
+			dequeue(queue);
+		}
+			enqueue(queueAux, dequeue(queue));
+	}
+
+	for(j = 0; j < sizeQueue; j++) {
+		enqueue(queue, dequeue(queueAux));
+	}
+}*/
+
+int removeValue(TQueue* queue, int input) {
+	if (queue == empty(queue)){
+		printf("Fila esta vazia...");
+		return;
+	}
+	
+	int i = queueSize(queue) - 1;
+	int j;
+	for(j = 0; queueFrontPosition(queue) != input; i--, j++) {
+		enqueue(queue, dequeue(queue));
+		if(j == queueSize(queue) - 1) {
+			return;
+		} 
+	}
+	
+	dequeue(queue);
+
+	for(i = i; i > 0; i--) {
+		enqueue(queue, dequeue(queue));
+	}
+		
+}
+
+void invert(TQueue* queue) {
+	int i, size = queueSize(queue);
+	int vector[size];
+	for(i = 0; !empty(queue); i++)
+		vector[i] = dequeue(queue);
+
+		
+	for(i = size; i >= 0; i--)
+		enqueue(queue, vector[i]);
+
+}
+
+
+int main(int argc, char*argv[]) {
+	TQueue* queue = neww();	
+
+	enqueue(queue, 7);
+	enqueue(queue, 8);
+	enqueue(queue, 5);
+	enqueue(queue, 9);
+	enqueue(queue, 10);
+	
+	show(queue);
+	/*
+	removeValue(queue, 11);
+	removeValue(queue, 12);
+	show(queue);*/
+	
+	
+	/*removeValue(queue, 4);
+	removeValue(queue, 10);
+	show(queue);*/
+	
+
+	invert(queue);
+	show(queue);
+	
 }
--- queue/Activity 1/queue.c
+++ queue/Activity 1/queue.c
@@ -1,13 +1,107 @@
-typedef struct Queue TQueue;
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
 
-TQueue* neww();
-int dequeue(TQueue* queue);
-void enqueue(TQueue* queue, int value);
-void show(TQueue* queue);
-int size(TQueue* queue);
-int start(TQueue* queue);
-int empty(TQueue* queue);
-int full(TQueue* queue);
-int queueFrontPosition(TQueue* queue);
-void destruct(TQueue *queue);
-void queueInvert(TQueue *queue);
+struct Queue {
+	int vector[5];
+	int start, final, quantity;
+};
+
+TQueue *neww() {
+	TQueue* queue = (TQueue*)malloc(sizeof(TQueue));
+	queue->start = -1;
+	queue->final = -1;
+	queue->quantity = 0;
+
+	return queue;	
+}
+
+void enqueue(TQueue *queue, int value) {
+	if ( ! ((queue -> final + 1) % 5 == queue -> start)) {
+		if (queue -> start == -1) queue -> start = 0;
+		queue -> final = (queue -> final + 1) % 5;
+		queue -> vector[queue -> final] = value;
+		queue -> quantity++;
+	}
+}
+
+int dequeue(TQueue *queue) {
+	int queuing = -1;
+	if (queue -> start > -1) {
+		queuing = queue -> vector[queue -> start];
+		if (queue -> start == queue -> final) {
+			queue -> start = queue -> final = -1;
+		} else {
+		queue -> start = (queue -> start + 1) % 5;
+		queue -> quantity--;
+		}
+	}
+	return queuing;
+}
+
+int queueSize(TQueue *queue) {
+	return queue -> quantity;
+}
+
+int queueFrontPosition(TQueue *queue) {
+	if (queue -> start > -1) {
+		return queue -> vector[frontPosition(queue)];
+	}
+}
+
+int empty(TQueue *queue) {
+	if (queue -> start == -1) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+int full(TQueue *queue) {
+	if (((queue->final + 1) % 5 == queue->start)) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
+
+void show(TQueue *queue) {
+	int i, index;
+	index = queue -> start;
+	printf("\nInicio: %i\n", queue -> vector[queue->start]);
+	printf("\nFinal: %i\n", queue -> vector[queue->final]);
+	for (i = 0; i < queue -> quantity; i++) {
+		printf("\n%i\n", queue -> vector[index]);
+		index = (index + 1) % 5;
+	}
+}
+
+int frontPosition(TQueue *queue) {
+	return queue -> start;
+}
+
+void destruct(TQueue *queue) {
+	if (queue != NULL){
+		free(queue);
+	}
+}
+
+void queueInvert(TQueue *queue) {
+	int size = queueSize(queue);
+	if (size == -1) {
+		printf("A Pilha esta vazia.\n");
+		return;
+	}
+
+	int i, inversor = size / 2, queueAux, start = queue -> start, final = queue -> final - 1;
+
+	for (i = 0; i < inversor; i++) {
+		queueAux = queue -> vector[start];
+		queue -> vector[start] = queue -> vector[final];
+		queue -> vector[final] = queueAux;
+		start = (start + 1) % 5;
+		final = (final - 1) % 5;
+	}
+}
