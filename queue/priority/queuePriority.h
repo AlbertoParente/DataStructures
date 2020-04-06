651ca45a164c36d0c291086184465e6660200eca
AlbertoParente <albertoparente20@gmail.com>
2020-04-06 00:20:16 -0300

    Adding function neww, queueSize, empty, queueQuantity, enqueue, dequeue

--- queue/priority/queuePriority.h
+++ queue/priority/queuePriority.h
@@ -1,1 +1,10 @@
+typedef struct QueuePriority TQueuePriority;
 
+TQueuePriority *neww();
+int queueSize(TQueuePriority *queue);
+int empty(TQueuePriority *queue);
+int full(TQueuePriority *queue);
+void enqueue(TQueuePriority *queue, int value);
+int dequeue(TQueuePriority *queue);
+void show(TQueuePriority *queue);
+void destruct(TQueuePriority *queue);
