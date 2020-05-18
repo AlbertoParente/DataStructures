a387a1096c475b15e604018bc960c59e56c1de9e
AlbertoParente <albertoparente20@gmail.com>
2020-05-18 20:11:27 -0300

    Adding methods in doublyLinkedList

--- list/doublyLinkedList/list.c
+++ list/doublyLinkedList/list.c
@@ -15,20 +15,101 @@ TListDoubly *create(int value) {
 	list -> previous = NULL;
 }
 
-int size(TListDoubly *list) {
+
+void insert(TListDoubly *list, int value) {
+	if(list == NULL) return;
+
+	TListDoubly *listAux = list;
+
+	while(listAux -> next != NULL){
+		listAux = listAux -> next;
+	}
+
+	TListDoubly *newElement = create(value);
 	
+	listAux -> next = newElement;
+	newElement -> previous = listAux;
+
 }
-void insert(TListDoubly *list, int value);
-int extract(TListDoubly *list, int value);
+
+int extract(TListDoubly *list, int value) {
+	if(list == NULL) return;
+
+	TListDoubly *listAux = list -> next;
+
+	while(listAux != NULL) {
+		if(listAux -> element == value) {
+			if(listAux -> next == NULL) {
+				listAux -> previous -> next = NULL;
+				free(listAux);
+			} else {
+				listAux -> previous -> next = listAux -> next;
+				listAux -> next -> previous = listAux -> previous;
+				free(listAux);
+			}			
+			return listAux -> element;
+		}
+		listAux = listAux -> next;
+	}
+}
 
 TListDoubly *search(TListDoubly *list, int value) {
 	if(list == NULL) return NULL;
-	TListDoubly *listAux = list -> next;
+
+	TListDoubly *listAux = list -> next;	
+
 	while(listAux != NULL) {
-		if(listAux -> element == value) return listAux;
-			listAux = listAux -> next
+		if(listAux -> element == value) {
+			return listAux;
+		}
+		listAux = listAux -> next;
 	}
 	return NULL;
 }
 
-void show(TListDoubly *list);
+void insertInPosition(TListDoubly *list, int value, int position) {
+	int counter = 0;
+
+	if(list == NULL) return;
+
+	TListDoubly *listAux = list;
+
+	while(counter < position) {		
+		listAux = listAux -> next;
+		counter++;
+	}
+
+	TListDoubly* newElement = create(value);
+
+	if (listAux -> next != NULL) newElement -> next = listAux -> next;
+	if (listAux -> next != NULL) listAux -> next -> previous = newElement;
+
+	listAux -> next = newElement;
+	newElement -> previous = listAux;
+}
+
+TListDoubly* removeInPosition(TListDoubly *list, int position){
+	int counter = 0;
+
+	if(list == NULL) return NULL;
+
+	TListDoubly* listAux = list;
+
+	while(counter <= position) {
+		if(listAux -> next == NULL) {			
+			return NULL;
+		}
+		listAux = listAux -> next;
+		counter++;
+	}
+	
+	if(listAux -> next == NULL) {
+		listAux -> previous -> next = NULL;
+		free(listAux);
+	} else {
+		listAux -> previous -> next = listAux -> next;
+		listAux -> next -> previous = listAux -> previous;
+		free(listAux);
+	}
+	return NULL;
+}
--- list/doublyLinkedList/list.h
+++ list/doublyLinkedList/list.h
@@ -6,3 +6,5 @@ void insert(TListDoubly *list, int value);
 int extract(TListDoubly *list, int value);
 TListDoubly *search(TListDoubly *list, int value);
 void show(TListDoubly *list);
+void insertInPosition(TListDoubly *list, int value, int position);
+TListDoubly* removeInPosition(TListDoubly *list, int position);
